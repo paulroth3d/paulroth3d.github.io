02509d4676b3f8ed03ea11dd796d013b1ef8adc9
Paul Roth <paulroth3d@gmail.com>
2019-03-15 16:03:32 -0500

    UPDATE: add in a stop playing handler avoid the case of the keys playing again when changing keys.

--- musicTest/precache-manifest.0b0857f86edbe6e974d7e269dce259db.js
+++ musicTest/precache-manifest.0b0857f86edbe6e974d7e269dce259db.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/musicTest/static/js/runtime~main.0cf311f6.js"
   },
   {
-    "revision": "ceeef8b37081a32cff60",
-    "url": "/musicTest/static/js/main.ceeef8b3.chunk.js"
+    "revision": "0e1318ae8ee738406d72",
+    "url": "/musicTest/static/js/main.0e1318ae.chunk.js"
   },
   {
     "revision": "63a7d94efc3a67c9cfb1",
     "url": "/musicTest/static/js/2.63a7d94e.chunk.js"
   },
   {
-    "revision": "ceeef8b37081a32cff60",
+    "revision": "0e1318ae8ee738406d72",
     "url": "/musicTest/static/css/main.3e971f3a.chunk.css"
   },
   {
-    "revision": "f35d4a000109fe503325f7a2759403f0",
+    "revision": "3f77681997b420eb7c4f47e62073ce7a",
     "url": "/musicTest/index.html"
   }
 ];
--- musicTest/service-worker.js
+++ musicTest/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/musicTest/precache-manifest.a8e4806aa7e9040c21031c73c1f0f7ff.js"
+  "/musicTest/precache-manifest.0b0857f86edbe6e974d7e269dce259db.js"
 );
 
 workbox.clientsClaim();
